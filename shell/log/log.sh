6e12027cb2d2e8a8a4854442359f6efa5cd0036b
lvyahui8 <lvyahui8@126.com>
2017-07-19 23:32:08 +0800

    update

--- shell/log/log.sh
+++ shell/log/log.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 
-log_path=/tmp/
+log_path=$1
 mkdir -p ${log_path}
 date_time=`date +"%Y%m%d"`
-log_file=${log_path}/keepalived-${date_time}.log
+log_file=${log_path}/$2-${date_time}.log
 touch ${log_file}
-
 
 function log() {
     local message="$1"
@@ -15,12 +14,12 @@ function log() {
     printf "[ %s ] [ %5d ] %s\n" "$ctime" $$ "$message"
 }
 
+export -f log
+
 function log2file(){
     local message="$1"
     local ctime=`date +"%Y-%m-%d %H:%M:%S"`
     #echo "[$ctime] $message" | tee -a "$log_file"
     printf "[ %s ] [ %5d ] %s\n" "$ctime" $$ "$message" | tee -a "$log_file"
 }
-
-log "lvyahui"
-log2file "hello \n world!"
+export -f log2file
