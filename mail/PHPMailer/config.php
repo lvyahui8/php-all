2e306b8aa712b7ae68c4cefb809a087f79211085
lvyahui8 <lvyahui8@126.com>
2017-10-24 00:23:24 +0800

    update

---
+++ mail/PHPMailer/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lvyahui
+ * Date: 2017/10/23
+ * Time: 22:36
+ */
+
+return array(
+    'smtp_host' =>  'smtp.qq.com',
+    'smtp_port' =>  587,
+    'svr_user'  =>  'xxx@qq.com',
+    'svr_pass'  =>  'xxx',
+    'default_addresses' =>  array(
+        '',
+    ),
+);
